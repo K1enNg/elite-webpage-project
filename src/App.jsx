a4914c8eddd2c7826c09a9e3ef5b23f32b221ed4
Kinkin <hiimkien2702@gmail.com>
2024-11-12 22:59:41 -0500

    modified router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import { Routes, Route, useLocation, Route } from 'react-router-dom'
+import { Routes, Route, useLocation} from 'react-router-dom'
 import Navbar from './components/navbar/Navbar';
 import News from './components/News/News';
+import Homepage from './components/homepage/homepage';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Categories from './components/Categories/Categories';
 const App = () => {
 
   return (
 
     <>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route index element={<Homepage/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/news' element={<News/>}/>
+        <Route path='/categories' element={<Categories/>}/>
+      </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider} from 'react-router-dom'
-import About from './components/About/About.jsx'
-import Contact from './components/Contact/Contact.jsx'
-import News from './components/News/News.jsx'
-import Categories from './components/Categories/Categories.jsx'
-import Homepage from './components/homepage/homepage.jsx'
+import { BrowserRouter} from 'react-router-dom'
+import App from './App.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Homepage/>
-  },
-  {
-    path: '/news',
-    element: <News/>
-  },
-  {
-    path: '/about',
-    element: <About/>
-  },
-  {
-    path: '/contact',
-    element: <Contact/>
-  },
-  {
-    path: '/categories',
-    element: <Categories/>
-  }
-])
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </StrictMode>,
 )
